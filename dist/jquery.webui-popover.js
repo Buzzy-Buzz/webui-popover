2bc59f140f4e250129eb84873bd8cb57be294c93
Adam Ginsburg <adam.ginsburg@buzzy.uzz>
2018-01-07 21:21:50 +1100

    change DIST too

--- dist/jquery.webui-popover.js
+++ dist/jquery.webui-popover.js
@@ -31,7 +31,7 @@
             height: 'auto',
             trigger: 'click', //hover,click,sticky,manual
             style: '',
-            selector: false, // jQuery selector, if a selector is provided, popover objects will be delegated to the specified. 
+            selector: false, // jQuery selector, if a selector is provided, popover objects will be delegated to the specified.
             delay: {
                 show: null,
                 hide: 300
@@ -953,7 +953,7 @@
                         height: this.$element[0].offsetHeight || elRect.height
                     });
                     // Else fixed container need recalculate the  position
-                } else if (cssPos === 'fixed') {
+                } else if (cssPos === 'fixed' || cssPos === 'absolute') {
                     var containerRect = container[0].getBoundingClientRect();
                     return {
                         top: elRect.top - containerRect.top + container.scrollTop(),
